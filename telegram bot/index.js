b0aa1a08a0556f19b9a2b0759e4a4fd65c03bc0c
Said <saidazizsaidakbarov@gmail.com>
2022-11-23 17:52:32 +0500

    file added

--- telegram bot/index.js
+++ telegram bot/index.js
@@ -5,16 +5,19 @@ const bot = new telegramBot(token, {
     polling: true
 })
 
-const arrMessagesBot = [ 'Nma kere jalab kech boganda?', 'Nma kere jalab kech boganda', 'Nma gap xatobow?'];
+const arrMessagesBot = ['Nma kere jalab kech boganda?', 'Nma kere jalab kech boganda', 'Nma gap xatobow?'];
 
-const roundMatch = ( max, min) => {
+const roundMatch = (max, min) => {
     return Math.round(min - 0.5 + Math.random() * (max - min + 1));
 };
 
 bot.on('message', msg => {
-    const {id: id, first_name: userName} = msg.chat;
+    const {
+        id: id,
+        first__name: userName
+    } = msg.chat;
 
-    if(/Hello/gi.test(msg.text)) {
+    if (/Hello/gi.test(msg.text)) {
         bot.sendMessage(id, `${arrMessagesBot[roundMatch(2, 0)]} ${userName}`);
     };
 });
